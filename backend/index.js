4669f5f8afea169c0e0701054955b5a3c7924f04
jionbiju <jionbiju@gmail.com>
2024-08-13 23:07:43 +0530

    admin panel navbar created

--- backend/index.js
+++ backend/index.js
@@ -40,6 +40,89 @@ app.post("/upload",upload.single('product'),(req,res)=>{
         image_url:`http://localhost:${port}/images/${req.file.filename}`,
     })
 })
+//Schema for creating Products
+
+const Product = mongoose.model("Product",{
+    id:{
+        type: Number,
+        required: true,
+    },
+    name:{
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    new_price:{
+        type:Number,
+        required: true,
+    },
+    old_price:{
+        type:Number,
+        required: true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    avilable:{
+        type: Boolean,
+        default:true,
+    }
+})
+
+
+app.post('/addproduct',async (req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0)
+    {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else{
+        id=1;
+    }
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+//Create API For delete Product
+app.post('/removeproduct', async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+//Creating API for  getting all products
+app.get('/allproducts', async (req,res)=>{
+    let products = await Product.find({});
+    console.log("All Product Fetched");
+    res.send(products);
+})
 
 app.listen(port,(error)=>{
     if(!error){
