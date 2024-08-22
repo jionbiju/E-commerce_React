e2489c88fe6cb033c349855ade1eea3e27702d63
jionbiju <jionbiju@gmail.com>
2024-08-22 19:26:02 +0530

    quick update

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -13,6 +13,20 @@ const ListProduct = () => {
   useEffect(()=>{
     fetchInfo();
   },[])
+  
+  const remove_product = async (id)=>{
+    await fetch('http://localhost:4000/removeproduct',{
+      method:'POST',
+      headers:{
+        Accept:'application/json',
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({id:id})
+    })
+    await fetchInfo();
+  }
+
+
   return (
     <div className='list-product'>
       <h1>Add Product List</h1>
@@ -31,10 +45,10 @@ const ListProduct = () => {
           <div key={index} className='listproduct-format-main listproduct-format'>
               <img src={product.image} alt=""className='listproduct-product-icon' />
               <p>{product.name}</p>
-              <p>${product.old_price}</p>
-              <p>${product.new_price}</p>
+              <p>$ {product.old_price}</p>
+              <p>$ {product.new_price}</p>
               <p>{product.category}</p>
-              <img className='listprodcut-remove-icon'src={cross_icon} alt="" />
+              <img onClick={()=>{remove_product(product.id)}} className='listprodcut-remove-icon'src={cross_icon} alt="" />
           </div>
           <hr />
           </>
